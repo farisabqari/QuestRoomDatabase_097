ecef25490808f46691c03cccc7acd58e6c48bbeb
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-17 10:52:39 +0800

    Menambahkan RepositoryMhs untuk operasi CRUD mahasiswa: insert, get all, get by nim, delete, dan update

--- app/src/main/java/com/example/pratikum_baru/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pratikum_baru/repository/RepositoryMhs.kt
@@ -1,8 +1,20 @@
 package com.example.pratikum_baru.repository
 
-
 import com.example.pratikum_baru.Data.Entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa) //interface ini operasinya harus sesuai dengan yang di dao
+
+    //getAllMhs
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    //getMhs
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    //deleteMhs
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    //updateMhs
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
