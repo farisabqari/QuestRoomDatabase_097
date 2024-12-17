45e2ceabcba86225c0a21bd225a4e0fa09c8538d
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-17 10:51:47 +0800

    menambahkan insertMhs, getAllMhs, getMhs, deleteMhs, dan updateMhs

--- app/src/main/java/com/example/pratikum_baru/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/pratikum_baru/repository/LocalRepositoryMhs.kt
@@ -1,13 +1,35 @@
 package com.example.pratikum_baru.repository
 
+
+
 import com.example.pratikum_baru.Data.Entity.Mahasiswa
 import com.example.pratikum_baru.Data.dao.MahasiswaDao
-
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs {
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+
+    //getAllMhs
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    //getMhs
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    //deleteMhs
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    //updateMhs
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }
