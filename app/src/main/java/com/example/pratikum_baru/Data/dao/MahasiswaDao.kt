e64f59ec80ea12f0595436ed4759761ce998e022
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-17 10:53:18 +0800

    menambhakan Delete dan update

--- app/src/main/java/com/example/pratikum_baru/Data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/pratikum_baru/Data/dao/MahasiswaDao.kt
@@ -1,14 +1,30 @@
 package com.example.pratikum_baru.Data.dao
 
 import androidx.room.Dao
-import androidx.room.Index
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.pratikum_baru.Data.Entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
     @Insert
-    suspend fun insertMahasiswa (
-        mahasiswa: Mahasiswa
-    )
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    //getAllMahasiswa
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
+    //getMahasiswa
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
+
+    //deleteMahasiswa
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }
